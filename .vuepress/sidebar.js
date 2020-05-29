5fe156edac8257f47f12a204e60ff51c5653284b
NTM Nathan <34463340+NTMNathan@users.noreply.github.com>
2020-05-30 09:06:38 +1000

    Compiled #4

--- .vuepress/sidebar.js
+++ .vuepress/sidebar.js
@@ -1,37 +1,49 @@
 const sidebar = {
-  "/docs/": [
-    "gettingstarted",
-    "faq",
-    {
-      title: "Examples",
-      children: [
-        "/docs/Examples/Avatar",
-        "/docs/Examples/MessageEmbed",
-        "/docs/Examples/Ping",
-      ],
-    },
-    {
-      title: "Guild",
-      children: [
-        "/docs/Guild/",
-        "/docs/Guild/positionswap",
-        "/docs/Guild/presence",
-      ],
-    },
-    {
-      title: "Member",
-      children: [
-        "/docs/Member/",
-        "/docs/Member/user",
-      ],
-    },
-    {
-      title: "TypeDefs",
-      children: [
-        "/docs/TypeDefs/snowflake",
-        "/docs/TypeDefs/ImageURLOptions",
-      ],
-    },
-  ],
-};
+	"/docs/": [
+		"gettingstarted",
+		"faq",
+		{
+			title: "Examples",
+			children: [
+				"/docs/Examples/Avatar",
+				"/docs/Examples/MessageEmbed",
+				"/docs/Examples/Ping"
+			],
+		},
+		{
+			title: "Channel",
+			children: [
+				"/docs/Channel/",
+			],
+		},
+		{
+			title: "Guild",
+			children: [
+				"/docs/Guild/",
+				"/docs/Guild/positionswap",
+				"/docs/Guild/presence"
+			],
+		},
+		{
+			title: "Member",
+			children: [
+				"/docs/Member/",
+				"/docs/Member/user"
+			],
+		},
+		{
+			title: "Role",
+			children: [
+				"/docs/Role/"
+			],
+		},
+		{
+			title: "TypeDefs",
+			children: [
+				"/docs/TypeDefs/snowflake",
+				"/docs/TypeDefs/ImageURLOptions",
+			],
+		},
+	]
+}
 module.exports = sidebar;
