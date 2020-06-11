79b63cbe7a23da7abd2db8c6cc1fc7e18dfc3857
NTM Nathan <ntmnathantominecraft@gmail.com>
2020-06-11 22:32:21 +1000

    big dramatic change

---
+++ .vuepress/sidebar.js
@@ -0,0 +1,16 @@
+const sidebar = {
+	"/docs/": [
+		"gettingstarted",
+		"faq",
+		{
+			title: "Examples",
+			children: [
+				"/docs/Examples/Avatar",
+				"/docs/Examples/MessageEmbed",
+				"/docs/Examples/Ping"
+			],
+		},
+
+	]
+}
+module.exports = sidebar;
